22198f660d05bc6301de8823308fe0312ca0e3b5
Rémi Fayolle <buzeeg@github>
2012-09-16 12:16:23 +0200

    fix for gingerbread

--- src/com/remifayolle/android/datasettings/DataSettingsActivity.java
+++ src/com/remifayolle/android/datasettings/DataSettingsActivity.java
@@ -3,6 +3,7 @@ package com.remifayolle.android.datasettings;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -14,7 +15,9 @@ public class DataSettingsActivity extends Activity {
         
         // Create Intent
         Intent i = new Intent(android.provider.Settings.ACTION_DATA_ROAMING_SETTINGS);
-        
+        if(Build.VERSION.SDK_INT < 16)
+            i.setClassName("com.android.phone", "com.android.phone.Settings");
+       
         // Start activity and catch potential exception
         try {
             startActivity(i);        	
